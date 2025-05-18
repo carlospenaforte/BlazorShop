3821e35d5eb6a1058865c2fa2d713ad2357496a2
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-18 09:58:01 -0300

    Entities created in API application

---
+++ BlazorShop.API/Entities/CarItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorShop.API.Entities
+{
+    public class CarItem
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public int CartId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ BlazorShop.API/Entities/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorShop.API.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
