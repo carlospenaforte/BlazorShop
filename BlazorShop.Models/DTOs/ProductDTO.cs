3a3751fc023bb3d2a97141916385c0b096cf1e35
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-20 13:25:12 -0300

    Connect the project references to BlazorShop.Models;
    Created DTO to cover sensitive data;
    
    One DTO for each one model from API, except User;

---
+++ BlazorShop.Models/DTOs/ProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Models.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
