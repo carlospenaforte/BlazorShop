9db41a1e620a3296423ed0ef6f7324266603e24a
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-24 16:27:53 -0300

    Mapping entities for DTO to caritem and product, list and item in the cart

--- BlazorShop.API/Mappings/MappingDTOs.cs
+++ BlazorShop.API/Mappings/MappingDTOs.cs
@@ -32,5 +32,58 @@ namespace BlazorShop.API.Mappings
                         CategoryName = product.Category.Name,
                     }).ToList();
         }
+
+        public static ProductDTO ConvertProductToDTO(
+            this Product product)
+        {
+            return new ProductDTO
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                ImageURL = product.ImageURL,
+                Quantity = product.Quantity,
+                CategoryId = product.Category.Id,
+                CategoryName = product.Category.Name,
+            };
+        }
+
+        public static IEnumerable<CarItemDTO> ConvertCarItemsToDTO(
+            this IEnumerable<CarItem> carItems, IEnumerable<Product> products)
+        {
+            return (from CarItem in carItems
+                    join product in products
+                    on CarItem.ProductId equals product.Id
+                    select new CarItemDTO
+                    {
+                        Id = CarItem.Id,
+                        ProductId = CarItem.ProductId,
+                        ProductName = product.Name,
+                        ProductDescription = product.Description,
+                        ProductImageURL = product.ImageURL,
+                        ProductPrice = product.Price,
+                        CartId = CarItem.CartId,
+                        Quantity = CarItem.Quantity,
+                        ProductTotalPrice = product.Price * CarItem.Quantity,
+                    }).ToList();
+        }
+
+        public static CarItemDTO ConvertCarItemToDTO(
+            this CarItem carItem, Product product)
+        {
+            return new CarItemDTO
+            {
+                Id = carItem.Id,
+                ProductId = carItem.ProductId,
+                ProductName = product.Name,
+                ProductDescription = product.Description,
+                ProductImageURL = product.ImageURL,
+                ProductPrice = product.Price,
+                CartId = carItem.CartId,
+                Quantity = carItem.Quantity,
+                ProductTotalPrice = product.Price * carItem.Quantity,
+            };
+        }
     }
 }
