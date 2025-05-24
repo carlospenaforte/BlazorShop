73f29c87702bef278c3cb672a9a3ff5317ba1573
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-24 15:01:20 -0300

    Repositories API and dependencie injection

--- BlazorShop.API/Repositories/IProductRepositorie.cs
+++ BlazorShop.API/Repositories/IProductRepositorie.cs
@@ -1,7 +1,11 @@
-﻿namespace BlazorShop.API.Repositories
+﻿using BlazorShop.API.Entities;
+
+namespace BlazorShop.API.Repositories
 {
     public interface IProductRepositorie
     {
-
+        Task<IEnumerable<Product>> GetItems();
+        Task<Product> GetItem(int id);
+        Task<IEnumerable<Product>> GetItemsCategory(int id);
     }
 }
---
+++ BlazorShop.API/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using BlazorShop.API.Data;
+using BlazorShop.API.Entities;
+
+namespace BlazorShop.API.Repositories
+{
+    public class ProductRepository : IProductRepositorie
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItemsCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
