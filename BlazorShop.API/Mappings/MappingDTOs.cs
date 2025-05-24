c4d9e069f0c4bf85d5da8f0d34610fe950b239ad
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-24 16:16:39 -0300

    Connect to DTO in Controller Mapping the entities to DTO

---
+++ BlazorShop.API/Mappings/MappingDTOs.cs
@@ -0,0 +1,36 @@
+﻿using BlazorShop.API.Entities;
+using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.API.Mappings
+{
+    public static class MappingDTOs
+    {
+        public static IEnumerable<CategoryDTO> ConvertCategoryToDTO(
+            this IEnumerable<Category> categories)
+        {
+            return (from cateogie in categories select new CategoryDTO
+            {
+                Id = cateogie.Id,
+                Name = cateogie.Name,
+                IconCSS = cateogie.IconCSS,
+            }).ToList();
+        }
+
+        public static IEnumerable<ProductDTO> ConvertProductsToDTO(
+            this IEnumerable<Product> products)
+        {
+            return (from product in products
+                    select new ProductDTO
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Description = product.Description,
+                        Price = product.Price,
+                        ImageURL = product.ImageURL,
+                        Quantity = product.Quantity,
+                        CategoryId = product.Category.Id,
+                        CategoryName = product.Category.Name,
+                    }).ToList();
+        }
+    }
+}
