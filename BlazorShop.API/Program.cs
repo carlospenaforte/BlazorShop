a18b19ca1926190e9185082c7fda1d715598349c
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-24 16:01:46 -0300

    Added Scoped In program.cs

--- BlazorShop.API/Program.cs
+++ BlazorShop.API/Program.cs
@@ -1,4 +1,5 @@
 using BlazorShop.API.Data;
+using BlazorShop.API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductRepositorie, ProductRepository>();
 
 var app = builder.Build();
 
