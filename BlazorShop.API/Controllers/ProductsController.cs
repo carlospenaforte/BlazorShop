4e55f5a90139c3e0b68dd5aba266136160fbbf45
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-26 21:45:42 -0300

    get properities

--- BlazorShop.API/Controllers/ProductsController.cs
+++ BlazorShop.API/Controllers/ProductsController.cs
@@ -39,5 +39,45 @@ namespace BlazorShop.API.Controllers
                      $"Error retrieving data from the database: {ex.Message}");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetItem(int id)
+        {
+            try
+            {
+                var product = await _productRepositorie.GetItem(id);
+                if (product is null)
+                {
+                    return BadRequest("Error to locate the product");
+                }
+                else
+                {
+                    var productDTO = product.ConvertProductToDTO();
+                    return Ok(productDTO);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     $"Error retrieving data from the database: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetItemsCategory/{categoryId}")] 
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetItemsCategory(int categoryId)
+        {
+            try
+            {
+                var products = await _productRepositorie.GetItemsCategory(categoryId);
+                var productsDTO = products.ConvertProductsToDTO();
+                return Ok(productsDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     $"Error retrieving data from the database: {ex.Message}");
+            }
+        }
     }
 }
