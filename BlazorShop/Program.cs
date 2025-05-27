81f256417e588e3f22720f8ad49ce035f6d9b86f
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-27 13:27:57 -0300

    CONNECT THE SERVER TO PAGE

--- BlazorShop/Program.cs
+++ BlazorShop/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = "http://localhost:5261";
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 await builder.Build().RunAsync();
