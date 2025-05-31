f1b0291452103ae760a853745fa0a3f1f25b4293
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-31 18:18:57 -0300

    productservice inherits iproductservice
    get the variables httpclient and logger
    
    make async beacause of task from iproductservice
    
    using json

--- BlazorShop/Services/ProductService.cs
+++ BlazorShop/Services/ProductService.cs
@@ -1,14 +1,32 @@
 ﻿using BlazorShop.Models.DTOs;
+using System.Net.Http.Json;
 
 namespace BlazorShop.Services
 {
     public class ProductService : IProductService
     {
-        public Task<IEnumerable<ProductDTO>> GetItems()
+        public HttpClient _httpClient;
+        public ILogger<ProductService> _logger;
+
+        public ProductService(HttpClient httpClient, ILogger<ProductService> logger)
         {
-            // This method should return a list of products.
-            // For now, we will return an empty list.
-            return Task.FromResult<IEnumerable<ProductDTO>>(new List<ProductDTO>());
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetItems()
+        {
+            try
+            {
+                var productsDTO = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDTO>>("api/products");
+                return productsDTO;
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while fetching products.");
+                throw;
+            }
         }
     }
 }
